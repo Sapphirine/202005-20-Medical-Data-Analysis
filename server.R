6141160e0f9055c09151e470763d58982a946e56
Sirui Li <44909728+srli3@users.noreply.github.com>
2020-05-15 22:02:03 -0400

    Add files via upload
    Similarity and recommendation in R is implemented with smaller-scale data due to time consumed. Running python script could create complete data for visualization.

---
+++ server.R
@@ -0,0 +1,124 @@
+
+library(flexdashboard)
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(plotly)
+library(gridExtra)
+library(tibbletime)
+
+
+
+source("ui.R")
+server <- function(input, output, session) {
+    
+    ad_event <- read.csv("admissions_event.csv")
+    sim <- read.csv("similarity.csv")
+    
+    selectID <- reactive({
+        data.frame(
+            Name = c("ID"),
+            Value = input$s
+        )
+    })
+    
+    
+    # barplot admission_type
+    output$plot1 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=ADMISSION_TYPE))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    # barplot insurance
+    output$plot2 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=INSURANCE))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    # barplot LANGUAGE
+    output$plot3 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=LANGUAGE))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+
+    # barplot RELIGION
+    output$plot4 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=RELIGION))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    # barplot MARITAL STATUS
+    output$plot5 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=MARITAL_STATUS))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    # barplot ETHNICITY
+    output$plot6 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=ETHNICITY))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    # barplot admission_location
+    output$plot7 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=ADMISSION_LOCATION))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    # barplot discharge_location
+    output$plot8 <- renderPlot({
+        ggplot(data=ad_event)+
+            geom_bar(aes(x=DISCHARGE_LOCATION))+
+            theme(panel.background = element_blank())
+    }, height = 200)
+    
+    
+    
+    #table1
+    output$table1 <- renderTable({
+        ad_event %>%
+            filter(SUBJECT_ID==input$s) %>%
+            select(SUBJECT_ID,INSURANCE, LANGUAGE,RELIGION, MARITAL_STATUS, ETHNICITY) %>%
+            slice(1)
+    })
+    
+    output$table2 <- renderDataTable({
+        ad_event %>%
+            filter(SUBJECT_ID==input$s) %>%
+            select(DIAGNOSIS, ADMITTIME) %>%
+            distinct() %>%
+            arrange(ADMITTIME)
+    })
+    
+    output$table3 <- renderDataTable({
+        ad_event %>%
+            filter(SUBJECT_ID==input$s) %>%
+            select(SHORT_TITLE, ADMITTIME) %>%
+            distinct() %>%
+            arrange(ADMITTIME)
+    })
+    
+    output$table4 <- renderDataTable({
+        ad_event %>%
+            filter(SUBJECT_ID==input$s) %>%
+            select(HADM_ID,ICD9_CODE,ADMISSION_TYPE,ADMITTIME,DISCHTIME,SHORT_TITLE,LONG_TITLE,EDREGTIME,EDOUTTIME,DEATHTIME) %>%
+            distinct() %>%
+            arrange(ADMITTIME)
+    })
+    
+    output$table5 <- renderDataTable({
+        sim %>%
+            filter(SUBJECT_ID==input$p) %>%
+            select(-SUBJECT_ID)
+        
+    })
+    
+    
+}
---
+++ ui.R
@@ -0,0 +1,86 @@
+
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(plotly)
+library(dashboardthemes)
+library(flexdashboard)
+
+ad_event <- read.csv("admissions_event.csv")
+sim <- read.csv("similarity.csv")
+
+# Header
+header <- dashboardHeader(title = "Medical Dashboard")
+
+
+
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("Home", tabName = "home", icon = icon("hospital")),
+        menuItem("Patient Tracking", tabName="patient", icon = icon("id-card-alt")),
+        menuItem("Retrieving & Prediction", tabName = "pred", icon = icon("file-alt"))
+    )
+)
+
+body <- dashboardBody(
+    ### changing theme
+    shinyDashboardThemes(
+        theme = "grey_light"
+    ),
+    tabItems(
+        
+        tabItem(tabName = "home",
+                h2("MIMIC-III Critical Care Database: Beth Israel Deaconess Medical Center"),
+                fluidRow(
+                    infoBox(width = 4, "Total Patient Number", length(unique(ad_event$SUBJECT_ID)),icon = icon("clinic-medical"), color = "blue", fill = TRUE),
+                    infoBox(width = 4, "Total Admission Number", length(unique(ad_event$HADM_ID)), icon = icon("laptop-medical"), color = "purple", fill = TRUE),
+                    infoBox(width = 4, "Total Death Number", sum(unique(ad_event$DEATHTIME)!=""), icon = icon("hospital-user"), color = "black", fill = TRUE)
+                ),
+                fluidRow(
+                    box(plotOutput("plot1"), height = 220, width = 4, status = "success" ),
+                    box(plotOutput("plot2"), height = 220, width = 4, status = "primary"),
+                    box(plotOutput("plot3"), height = 220, width = 4, status = "warning")
+                ),
+                fluidRow(
+                    box(plotOutput("plot4"), height = 220, width = 4, status = "warning"),
+                    box(plotOutput("plot5"), height = 220, width = 4, status = "success"),
+                    box(plotOutput("plot6"), height = 220, width = 4, status = "primary")
+                ),
+                fluidRow(
+                    box(plotOutput("plot7"), height = 220, width = 6),
+                    box(plotOutput("plot8"), height = 220, width = 6)
+                )
+                
+                
+        ),
+        
+        
+        tabItem(tabName = "patient",
+                h2("Patient Information"),
+                fluidRow(box(title = "Select Patient", status = "primary",
+                             selectizeInput(
+                                 's', 'Search or Select Patient ID',
+                                 choices = unique(ad_event$SUBJECT_ID)), width = 3
+                             )
+                         ),
+                fluidRow(box(title="Personal Information" ,tableOutput('table1'), width = 12, solidHeader = TRUE)),
+                fluidRow(box(title = "Conditions", dataTableOutput("table2"), width = 6, status="warning",solidHeader = TRUE,collapsible=TRUE),
+                         box(title = "Treatments", dataTableOutput("table3"), width = 6, status="success",solidHeader = TRUE,collapsible=TRUE)),
+                fluidRow(box(title = "Medical Records", dataTableOutput("table4"),width = 12, solidHeader = TRUE,status = "primary"))
+        ),
+        tabItem(tabName = "pred",
+                h2("Similar Patient Retrieval and Diseases Prediction"),
+                fluidRow(box(title = "Select Patient", status = "primary",
+                             selectizeInput(
+                                 'p', 'Search or Select Patient ID',
+                                 choices = unique(sim$SUBJECT_ID)), width = 3)),
+                fluidRow(box(title = "Top 2 Most Similar Patients", dataTableOutput("table5"), solidHeader = T, status = "primary", width = 12)))
+    )
+)
+
+# Put them together into a dashboardPage
+dashboardPage(
+    header,
+    sidebar,
+    body
+)
